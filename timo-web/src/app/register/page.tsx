f3df0f7a4435d409bb1723ab11904f7a9c856a03
tsulsinisa14-maker <tsulsinisa@gmail.com>
2026-05-22 14:21:53 +0700

    fix login dan register

--- timo-web/src/app/register/page.tsx
+++ timo-web/src/app/register/page.tsx
@@ -1,1 +1,48 @@
-<div data-layer="Rectangle 1" class="Rectangle1 w-[1238px] h-[795.50px] opacity-95 bg-orange-100 rounded-[10px] shadow-[5px_4px_14px_0px_rgba(0,0,0,0.04)] shadow-[20px_16px_26px_0px_rgba(0,0,0,0.03)] shadow-[45px_36px_34px_0px_rgba(0,0,0,0.02)] shadow-[80px_64px_41px_0px_rgba(0,0,0,0.01)] shadow-[124px_100px_45px_0px_rgba(0,0,0,0.00)]"></div>
+<div data-layer="Register" class="Register w-[1440px] h-[1024px] relative bg-cyan-900 overflow-hidden">
+    <img data-layer="istockphoto-1483863447-2048x2048 1" class="Istockphoto14838634472048x20481 w-[1440px] h-[1024px] left-0 top-0 absolute" src="https://placehold.co/1440x1024" />
+    <div data-layer="Rectangle 1" class="Rectangle1 w-[1238px] h-[795.50px] left-[132px] top-[126px] absolute opacity-95 bg-orange-100 rounded-[10px] shadow-[5px_4px_14px_0px_rgba(0,0,0,0.04)] shadow-[20px_16px_26px_0px_rgba(0,0,0,0.03)] shadow-[45px_36px_34px_0px_rgba(0,0,0,0.02)] shadow-[80px_64px_41px_0px_rgba(0,0,0,0.01)] shadow-[124px_100px_45px_0px_rgba(0,0,0,0.00)]"></div>
+    <div data-layer="Frame 2" class="Frame2 w-24 h-96 left-[762px] top-[164px] absolute">
+        <div data-layer="Sign Up" class="SignUp w-40 h-11 left-[1.03px] top-0 absolute justify-start text-neutral-800 text-4xl font-bold font-['Montserrat']">Sign Up</div>
+        <div data-layer="I agree to all terms" class="IAgreeToAllTerms w-40 h-5 left-[45.42px] top-[577.29px] absolute justify-start text-neutral-800 text-base font-medium font-['Montserrat']">I agree to all terms</div>
+        <div data-layer="Rectangle 4" class="Rectangle4 size-5 left-0 top-[577.29px] absolute border border-neutral-600"></div>
+        <div data-layer="Rectangle 3" class="Rectangle3 w-[577px] h-16 left-0 top-[400.84px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="mdi:password" class="MdiPassword size-8 left-[18.58px] top-[416.14px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector w-5 h-7 left-[5.16px] top-[1.27px] absolute bg-neutral-800"></div>
+        </div>
+        <div data-layer="Enter Password" class="EnterPassword w-32 h-5 left-[72.25px] top-[421.24px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Enter Password</div>
+        <div data-layer="Rectangle 3" class="Rectangle3 w-[577px] h-16 left-0 top-[484.47px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="mdi:password-outline" class="MdiPasswordOutline size-8 left-[18.58px] top-[499.77px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector w-5 h-7 left-[5.16px] top-[1.27px] absolute bg-black"></div>
+        </div>
+        <div data-layer="Confirm Password" class="ConfirmPassword w-40 h-5 left-[72.25px] top-[504.87px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Confirm Password</div>
+        <div data-layer="Rectangle 3" class="Rectangle3 w-[577px] h-16 left-0 top-[317.20px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="ic:baseline-email" class="IcBaselineEmail size-8 left-[18.58px] top-[332.50px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector w-6 h-5 left-[2.58px] top-[5.10px] absolute bg-black"></div>
+        </div>
+        <div data-layer="Enter Email" class="EnterEmail w-24 h-5 left-[72.25px] top-[337.60px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Enter Email</div>
+        <div data-layer="Rectangle 3" class="Rectangle3 w-[577px] h-16 left-0 top-[149.93px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="icon-park-outline:edit-name" class="IconParkOutlineEditName size-7 left-[18.58px] top-[166.25px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector size-2 left-[10.24px] top-[2.38px] absolute outline outline-4 outline-offset-[-2px] outline-black"></div>
+            <div data-layer="Vector" class="Vector w-6 h-2.5 left-[2.41px] top-[14.87px] absolute outline outline-4 outline-offset-[-2px] outline-black"></div>
+        </div>
+        <div data-layer="Enter Last Name" class="EnterLastName w-36 h-5 left-[72.25px] top-[170.33px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Enter Last Name</div>
+        <div data-layer="Rectangle 2" class="Rectangle2 w-[577px] h-16 left-0 top-[233.57px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="mdi:user" class="MdiUser size-8 left-[18.58px] top-[248.87px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector size-5 left-[5.16px] top-[5.10px] absolute bg-neutral-800"></div>
+        </div>
+        <div data-layer="Enter Username" class="EnterUsername w-36 h-5 left-[72.25px] top-[253.97px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Enter Username</div>
+        <div data-layer="Rectangle 2" class="Rectangle2 w-[577px] h-16 left-0 top-[66.30px] absolute rounded-lg border border-neutral-600"></div>
+        <div data-layer="icon-park-solid:edit-name" class="IconParkSolidEditName size-7 left-[18.58px] top-[82.62px] absolute overflow-hidden">
+            <div data-layer="Vector" class="Vector size-2 left-[10.24px] top-[2.38px] absolute bg-black outline outline-4 outline-offset-[-2px] outline-black"></div>
+            <div data-layer="Vector" class="Vector size-2.5 left-[2.41px] top-[14.87px] absolute outline outline-4 outline-offset-[-2px] outline-black"></div>
+            <div data-layer="Vector" class="Vector size-2 left-[16.26px] top-[16.66px] absolute bg-black outline outline-4 outline-offset-[-2px] outline-black"></div>
+        </div>
+        <div data-layer="Enter First Name" class="EnterFirstName w-36 h-5 left-[72.25px] top-[86.70px] absolute justify-start text-neutral-400 text-base font-medium font-['Montserrat']">Enter First Name</div>
+        <div data-layer="Already have an account? Sign In" class="AlreadyHaveAnAccountSignIn w-72 h-5 left-[1.03px] top-[695.60px] absolute justify-start"><span class="text-neutral-800 text-base font-medium font-['Montserrat']">Already have an account? </span><span class="text-sky-600 text-base font-medium font-['Montserrat']">Sign In</span></div>
+        <div data-layer="Register" class="Register w-32 h-16 left-0 top-[616.05px] absolute">
+            <div data-layer="Rectangle 5" class="Rectangle5 w-32 h-16 left-0 top-0 absolute bg-cyan-900/80 rounded-[5px]"></div>
+            <div data-layer="Register" class="Register left-[33px] top-[21px] absolute justify-start text-orange-100 text-base font-medium font-['Montserrat']">Register</div>
+        </div>
+    </div>
+    <img data-layer="R 2" class="R2 w-96 h-[652px] left-[143px] top-[266px] absolute" src="https://placehold.co/433x652" />
+</div>
